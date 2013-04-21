761484697a8e327d8e3d5e21765c1551feb7571d
Ariel Ben Horesh <ariel_bh@yahoo.com>
2013-04-22 01:13:01 +0300

    Step 1: Activation Demo

--- RightMyGuide.WindowsPhone/Views/RootPivotView.xaml.cs
+++ RightMyGuide.WindowsPhone/Views/RootPivotView.xaml.cs
@@ -19,10 +19,41 @@ namespace RightMyGuide.WindowsPhone.Views
         {
             InitializeComponent();
             ViewModel = new RootPivotViewModel();
+            PhoneApplicationService.Current.Deactivated += AppDeactivated;
+            PhoneApplicationService.Current.Activated += AppActivated;
 
+            Loaded += delegate
+            {
+                int index;
+                if (IsolatedStorageSettings.ApplicationSettings.TryGetValue("mainPivotIndex", out index))
+                    pivot.SelectedIndex = index;
+            };
         }
 
-    
+        private void AppActivated(object sender, ActivatedEventArgs e)
+        {
+            RestorePivotIndex();
+        }
+
+        void AppDeactivated(object sender, DeactivatedEventArgs e)
+        {
+            IsolatedStorageSettings.ApplicationSettings["mainPivotIndex"] = pivot.SelectedIndex;
+        }
+
+        private void RestorePivotIndex()
+        {
+            if (PhoneApplicationService.Current.State.ContainsKey("mainPivotIndex"))
+            {
+                pivot.SelectedIndex = (int)PhoneApplicationService.Current.State["mainPivotIndex"];
+            }
+        }
+  
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            PhoneApplicationService.Current.State["mainPivotIndex"] = pivot.SelectedIndex;
+        }
 
         private void Pivot_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
